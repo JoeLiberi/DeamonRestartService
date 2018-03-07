da5c5f4e38a49a7e411647fbb8082b6378c36e2f
Joe Liberi <m4punk@gmail.com>
2018-03-06 21:57:27 -0500

    edited config example

--- config_example.py
+++ config_example.py
@@ -7,9 +7,24 @@ services = {
 	"proccessname" : "/some/path/to/script/script.sh"
 }
 
+email_to = ["email1@gmail.com", "email2@labworthy.com"]
+
+email_text = """\
+Subject: {subject}
+
+
+From: {sent_from}  
+To: {to}  
+
+
+{body}
+
+
+"""
+
 email_body = """\
 
 	Restarted the following proccesses succesfully:
 		{services} at {timestamp}
-		
+
 """
